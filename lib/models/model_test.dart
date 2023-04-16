8cebf5a38a69f3ef15a4fa6bd03cbd4091f8255b
Norberto <n.g.vasconcelos@gmail.com>
2023-04-16 01:43:25 +0100

    updated

--- lib/models/model_test.dart
+++ lib/models/model_test.dart
@@ -1,10 +1,14 @@
 /// Test class
 class ModelTest {
   final String text;
+  final String imageUrl;
 
   /// Constructor
-  ModelTest(this.text);
+  ModelTest(this.text, this.imageUrl);
 
   /// Function
   _testFunction() {}
+
+  /// Function 2
+  _testFunction2() {}
 }
